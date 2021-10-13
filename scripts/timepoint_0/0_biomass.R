8ae5def53f5239b59deb36e1293cd86eb4c75928
Dennis Conetta <dconetta1@my.uri.edu>
2021-10-13 14:46:09 -0400

    fixing the output command at the end

--- scripts/timepoint_0/0_biomass.R
+++ scripts/timepoint_0/0_biomass.R
@@ -24,17 +24,18 @@ sa <- read.csv("output/0_surface_area.csv")
 metadata <- read_csv("metadata/coral_metadata.csv") %>% select(1:3)
 
 #colony_id column messed up in naming so go back and name it again
-#homog_vol <- homog_vol %>%
-#  mutate(colony_id = ?..colony_id)
+homog_vol <- homog_vol %>%
+  mutate(colony_id = ï..colony_id)
 
 # Join homogenate volumes and surface area with sample metadata
 Data <- full_join(Data, homog_vol) %>%
   full_join(sa) %>% full_join(metadata)
 
-
+#Filter out just the Acropora species
 Data <- Data%>% filter(species=="Acropora")
+
 Data <- Data %>%
-  mutate(dry.pan.mass.g.vol.corr = dry.pan.mass.g/vol.added.ml* homog_vol_ml, #
+  mutate(dry.pan.mass.g.vol.corr = dry.pan.mass.g/vol.added.ml* homog_vol_ml,
          burnt.pan.mass.g.vol.corr = burnt.pan.mass.g/vol.added.ml *homog_vol_ml)
 
 
@@ -46,6 +47,6 @@ Data <- Data %>%
 
 Data <- Data %>%
   mutate(burnt.biomass.g = (dry.pan.mass.g.vol.corr - burnt.pan.mass.g.vol.corr ),
-         AFDW.mg.cm2 = ((burnt.biomass.g)*1000)/ surface.area.cm2)
+         AFDW.mg.cm2 = ((burnt.biomass.g)*1000)/ surface.area.cm2) %>%
+  write_csv(path = "output/0_biomass_output.csv")
 
-write_csv(Data, "output/0_biomass_output.csv")
