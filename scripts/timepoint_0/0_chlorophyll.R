994df7d524072f4aef30c7fb801a097a307c64df
Dennis Conetta <dconetta1@my.uri.edu>
2021-10-13 13:08:31 -0400

    finalized chl script for TP0

---
+++ scripts/timepoint_0/0_chlorophyll.R
@@ -0,0 +1,119 @@
+#Chlorophyll Concentration Code (merging E5_ROL with urol_e5 scripts for Oct 2019 data)
+#Script edited from urol_e5 repository to fit TP_0 (Ariana Huffmeyer and Dennis Conetta)
+
+rm(list=ls()) #clears workspace 
+
+## install packages if you dont already have them
+if (!require("tidyverse")) install.packages("tidyverse")
+if (!require("plotrix")) install.packages("plotrix")
+
+# load packages
+library(tidyverse)
+library(plotrix)
+
+#set working directory and save tibble
+
+setwd("C:/Users/Dennis/Documents/Github/Apulchra_Plasticity")
+
+chl_data <- read.csv("data/timepoint_0/0_chlorophyll/0_20191031_Chl.csv")
+
+# Define function to read in chl data
+
+read_chl <- function(file) {
+  chl_data <- read_csv(file, skip = 24, n_max = 24) %>%
+    select(-1) %>%
+    magrittr::set_colnames(c("row", 1:12, "wavelength")) %>%
+    fill(row) %>%
+    gather("col", "absorbance", -wavelength, -row) %>%
+    unite("well", c(row, col), sep = "")
+}
+
+# List chlorophyll data files
+chl_path <- "data/timepoint_0/0_chlorophyll/"                                        # Path to chlorophyll data directory
+all_chl_files <- list.files(path = chl_path, pattern = "*.csv")          # List all files in directory
+chl_platemaps <- list.files(path = chl_path, pattern = "platemap")       # List platemap files
+chl_data_files <- setdiff(all_chl_files, chl_platemaps)                  # List absorbance data files
+
+# Read in all files into tibble
+df <- tibble(file = chl_data_files) %>%
+  mutate(platemap = map(file, ~ read_csv(paste0(chl_path, tools::file_path_sans_ext(.), "_platemap.csv"))),
+         chl_data = map(file, ~ read_chl(paste0(chl_path, .))))
+
+# Merge platemap and data for each plate
+df <- df %>%
+  mutate(merged = map2(platemap, chl_data, ~ right_join(.x, .y)))
+
+# average all technical replicates for each plate/sample/wavelength, including all acetone blanks together (per plate)
+df <- df %>%
+  unnest(merged) %>%
+  filter(!is.na(colony_id)) %>%                         # remove empty wells (colony_id is NA)
+  group_by(file, colony_id, wavelength) %>%
+  summarise(n = n(), mean_abs = mean(absorbance)) %>%
+  spread(wavelength, mean_abs)
+
+
+# get the acetone blank 750 absorbace for each file (i.e., plate), and subtract from 630 and 663 values for each sample
+df <- df %>%
+  group_by(file) %>%
+  mutate(blank750 = `750`[colony_id == "BK"]) %>%
+  ungroup() %>%
+  mutate(adj630 = `630` - blank750,
+         adj663 = `663` - blank750)
+
+# calculate chla and chlc2 values based on equations from Jeffrey and Humphrey 1975
+# units µg/ml
+df <- df %>%
+  mutate(chla.ug.ml = 11.43 * adj663 - 0.64 * adj630,
+         chlc2.ug.ml = 27.09 * adj630 - 3.63 * adj663)
+
+
+# Load homogenate volume
+
+homog.vol <- read_csv("data/timepoint_0/0_homogenate_vols/0_homogenate_vols.csv") %>%
+  select(colony_id, homog_vol_ml)
+
+#merge df and homog.vol to normalize to total volume of homogenate
+chl_data <- full_join(df, homog.vol)
+
+
+# Load surface area
+sa <- read_csv("output/0_surface_area.csv")
+chl_data <- full_join(chl, sa)
+
+
+# Multiply chlorophyll by the homogenate volume and divide by surface area
+chl_data <- chl_data %>%
+  mutate(chla.ug.cm2 = chla.ug.ml * homog_vol_ml / surface.area.cm2,
+         chlc2.ug.cm2 = chlc2.ug.ml * homog_vol_ml / surface.area.cm2)
+
+# remove blanks and NAs
+chl_data <- filter(chl_data, !colony_id %in% c("NA", "BK", "Water"))
+
+#read in coral_metadata and merge with species
+metadata <- read.csv("metadata/coral_metadata.csv")
+
+#rename sample_id column because it is having issues
+metadata <- metadata %>%
+  mutate(colony_id = ï..colony_id) %>%
+  select(colony_id, species, site, genotype, purpose)
+
+chl_data <- full_join(chl_data, metadata)
+
+#filter out for only Acropora species
+chl_data <- chl_data %>%
+  filter(species == "Acropora")
+  
+# write chlorophyll data to output csv file
+chl_data %>%
+  select(colony_id, site, chla.ug.cm2, chlc2.ug.cm2) %>%
+  mutate(timepoint="timepoint0")%>%
+  write_csv(path = "output/0_chlorophyll.csv")
+
+
+
+
+
+
+
+
+
