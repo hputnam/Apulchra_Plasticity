f7e208e512a91a50c7cb6baea1de5fd12c52baec
Dennis Conetta <dconetta1@my.uri.edu>
2021-10-18 10:55:18 -0400

    starting to compile an output file for all tp0 univariate data

---
+++ scripts/timepoint_0/0_univariate_output.R
@@ -0,0 +1,13 @@
+#Summary of TP0 Univariate Response Variables (Except AQY, Am, and R)
+library(tidyverse)
+library(ggpubr)
+library(showtext)
+
+#All saved Figures composed into one figure
+Univariate_Summary_Fig <- ggarrange(Fig.1,Fig.2,Fig.3,Fig.4,Fig.5,Fig.6, ncol = 2, nrow = 3)
+
+#Make Summary Table of all ANOVA's and TukeyHSD
+#All data was log transformed to meet the assumptions of normality
+
+
+ggsave("output/TP0_Univariate_Figs.pdf", Fig, width=6, height=6)
