1ee45dc20dac0e7109df0d69ad68f76ae40ebc00
Dennis Conetta <dconetta1@my.uri.edu>
2021-10-18 10:14:00 -0400

    saving figs for larger figure comparison

--- scripts/timepoint_0/0_protein_holobiont.R
+++ scripts/timepoint_0/0_protein_holobiont.R
@@ -129,23 +129,23 @@ holo_prot <- holo_prot %>%
    prot_ug.cm2 = mean(prot_ug.cm2, na.rm = T)) 
 
 #Plot of the Data for each Site
-holo_prot %>%
+Fig.4 <- holo_prot %>%
   ggplot(aes(x = site, y = prot_ug.cm2, color = site)) +
-  #coord_cartesian(ylim = c(0, 0.5))+
-  labs(x = "Site", y = "Total protein (Âµg/cm2)", color = "Site") +
+  coord_cartesian(ylim = c(0, 600))+
+  labs(x = "Site", y = "Total Holobiont protein (µg/cm2)", color = "Site") +
   geom_jitter(width = 0.1) +                                            # Plot all points
   stat_summary(fun.data = mean_cl_normal, fun.args = list(mult = 1),    # Plot standard error
                geom = "errorbar", color = "black", width = 0.5) +
   stat_summary(fun = mean, geom = "point", color = "black")           # Plot mean
 
 #Quick Stats on Site vs Protein Content for all the corals
-model2 <- aov(log10(prot_ug.cm2) ~ site, data = holo_prot)
-anova(model2)
-TukeyHSD(model2)
+model4 <- aov(log10(prot_ug.cm2) ~ site, data = holo_prot)
+anova(model4)
+TukeyHSD(model4)
 par(mfrow=c(2,2))
-boxplot(model2$residuals)
-hist(model2$residuals)
-plot(model2$fitted.values, model2$residuals)
+boxplot(model4$residuals)
+hist(model4$residuals)
+plot(model4$fitted.values, model4$residuals)
 
 # All sites
 holo_prot %>%
