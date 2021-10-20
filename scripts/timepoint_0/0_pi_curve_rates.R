08306b5f6c0697d507711e0ad2bf948861811e44
Dennis Conetta <dconetta1@my.uri.edu>
2021-10-20 10:40:43 -0400

    updating pi curve rate script as making changes

--- scripts/timepoint_0/0_pi_curve_rates.R
+++ scripts/timepoint_0/0_pi_curve_rates.R
@@ -24,9 +24,25 @@ library(furrr)
 #Set Working Directory
 setwd("C:/Users/Dennis/Documents/Github/Apulchra_Plasticity")
 
+#edit down metadata file and output just for ACR
+pi_metadata <- read.csv("data/timepoint_0/0_pi_curves/All_PI_Curve_Sample_Info.csv")
+
+acr_only_data <- pi_metadata %>%
+  mutate(Date = ï..Date) %>%
+  filter(Date == 20191024 | Date == 20191025 | Date == 20191029) %>%
+  filter(Species == "Acropora" | Species == "Blank") %>%
+  write_csv(path = "data/timepoint_0/0_pi_curves/acr_only_pi_curve_metadata.csv")
+  
 
 path.p <- "data/timepoint_0/0_pi_curves/" #the location of all your respirometry files 
 
 # List data files
 file.names <- list.files(path = path.p, pattern = "csv$")  # list all csv file names in the folder
 file.names <- file.names[!grepl("metadata", file.names)]   # omit metadata from files to be read in as data
+
+
+# Load PI curve sample metadata (i.e., which corals were in which runs)
+sample.info <- read_csv(file = "data/timepoint_0/0_pi_curves/acr_only_pi_curve_metadata.csv")
+
+# Load PI curve run metadata (i.e., light levels and interval times for each run)
+run.info <- read_csv(file = "data/1_pi_curves/1_pi_curves_run_metadata.csv")
